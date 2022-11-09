4d886399619b407e6fb305d9d4aedd7e82134adf
Diego Cruz Lugo <18170307@itculiacan.edu.mx>
2022-11-08 18:10:42 -0700

    test de componentes

---
+++ src/components/ComponenteBase.jsx
@@ -0,0 +1,15 @@
+export function Nombre(params) {
+  return (
+    <div>
+      <h1>Nombre</h1>
+    </div>
+  );
+}
+
+export function Nombre2(params) {
+    return (
+      <div>
+        <h1>Nombre2</h1>
+      </div>
+    );
+  }
