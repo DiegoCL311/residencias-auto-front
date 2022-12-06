7f92f4cd72eaf4223c0fec8f59c9126c24271823
Diego Cruz <18170307@itculiacan.edu.mx>
2022-12-05 21:25:32 -0700

    WIP pagina todos proyectos y revisar 1 proyecto

--- src/pages/EvaluacionAcademia.jsx
+++ src/pages/EvaluacionAcademia.jsx
@@ -1,106 +1,101 @@
 import {
-    Grid,
-    Typography,
-    Box,
-    Paper,
-    TextField,
-    Autocomplete,
-    Chip,
-    
-  } from "@mui/material";
-  import MyStepper from "../components/MyStepper";
-  import Button from '@mui/material/Button';
-  import * as React from "react";
+  Grid,
+  Typography,
+  Box,
+  Paper,
+  TextField,
+  Autocomplete,
+  Chip,
+} from "@mui/material";
+import MyStepper from "../components/MyStepper";
+import Button from "@mui/material/Button";
+import * as React from "react";
 import { green } from "@mui/material/colors";
+import { Route, Link, Routes, useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
+export function EvaluacionAcademia(props) {
+  const params = useParams();
 
- 
-  export function EvaluacionAcademia(props) {
-    const [formDatos, setFormDatos] = React.useState({
-      asesoriaNumero: "",
-      tipoAsesoria: "",
-      temaAsesoria: [],
-      solucion: "",
-    });
-  
-    function changeValueDatos(params) {
-      setFormDatos({
-        ...formDatos,
-        [params.target.id]: params.target.value,
-      });
+  const [proyecto, setProyecto] = React.useState({});
+
+  useEffect(() => {
+    async function fetchProyecto() {
+      try {
+        let response = await axios({
+          method: "get",
+          url: "http://localhost:3002/proyectos/" + params.id,
+        }).then((response) => {
+          setProyecto(response.data.recordset[0]);
+        });
+
+        //setProyectos(...proyectos, response.data.recordset);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  
-    const [activeStep, setActiveStep] = React.useState(0);
-  
-    function hazAlgo() {
-      console.table(formDatos);
-    }
-  
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-  
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-    
-    
-    return (
-      <Grid>
-        <Paper
-          elevation={8}
-          sx={{
-            px: 16,
-            py: 4,
-            mx: "auto",
-            my: "4%",
-            width: "60%",
-            height: "100%",
-            overflow: "auto",
-          }}
-        >  
+
+    fetchProyecto();
+  }, []);
+
+  return (
+    <Grid>
+      <Paper
+        elevation={8}
+        sx={{
+          px: 16,
+          py: 4,
+          mx: "auto",
+          my: "4%",
+          width: "60%",
+          height: "100%",
+          overflow: "auto",
+        }}
+      >
+        {/*
+        
+        "ID_PROYECTO": 1,
+        "ProyectoTITULO": "asd",
+        "ProyectoRealizacion": "asd",
+        "ID_ASESOR_EXTERNO": 8,
+        "ID_EMPRESA": 11,
+        "ID_STATUS": 1,
+        "EMPRESA_NOMBRE": "Nombre",
+        "EXTERNO_NOMBRE": "Elvis tek"
+        
+        */}
         <Typography variant="h4" sx={{ textAlign: "center", my: "2vh" }}>
-        Evaluación Del Proyecto
-      </Typography>
+          Evaluación Del Proyecto
+        </Typography>
 
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $NombreEmpresa
-      </Typography>
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $NombreAseorExterno
-      </Typography>
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $objetivosProyecto
-      </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
+          nombre de la empresa: {proyecto.EMPRESA_NOMBRE}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
+          Nombre del asesor externo: {proyecto.EXTERNO_NOMBRE}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
+          Titulo del proyecto: {proyecto.ProyectoTITULO}
+        </Typography>
 
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $objetivosEspecificos
-      </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
+          Realizacion del propyecto: {proyecto.ProyectoRealizacion}
+        </Typography>
 
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-       $delimitacionProyecto
-      </Typography>
-
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $justificacionProyecto
-      </Typography>
-
-      <Typography variant="subtitle1" sx={{ textAlign: "center", my: "2vh" }}>
-        $cronogramaActividades
-      </Typography>
-
-          
-
-     
-              <Button color="warning" variant="contained" >Rechazar</Button>
-              <div className="button1">
-              <Button color="primary" variant="contained" >Aceptar</Button>
-              </div>
-
-
-          
-        </Paper>
-      </Grid>
-    );
-  }
-  
+        <Grid container>
+          <Grid item xs={6}>
+            <Button color="primary" variant="contained">
+              Aceptar
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button color="warning" variant="contained">
+              Rechazar
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+}
